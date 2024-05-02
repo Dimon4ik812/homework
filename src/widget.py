63d1c8dd52db34bd84e768573789367df7855447
Дмитрий <kriukov@pos-credit.ru>
2024-05-02 21:13:00 +0300

    Final version

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,10 @@
 from src.masks import account_disguise, masking_card_number
 
+
 def data_masking_function(user_data: str) -> str:
     """функциия принимает тип карты/счета и номер/карты счета и маскирует их"""
     if 'Счет'.lower() in user_data.lower():
-        result = account_disguise(user_data)
+        result = account_disguise(int(user_data))
         return result
     else:
         card_type, card_number = user_data.split(maxsplit=1)
@@ -11,7 +12,7 @@ def data_masking_function(user_data: str) -> str:
         return card_type + ' ' + filter_card_number
 
 
-print(data_masking_function('Maestro 1596837868705199'))
-
-
-
+def date_decoding(encrypted_date: str) -> str:
+    """функция принимает дату и переворачивает ее"""
+    data_parts = encrypted_date.split('T')[0].split('-')
+    return '.'.join(data_parts[::-1])
