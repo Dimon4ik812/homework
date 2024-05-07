54f2d7707b6b3d233924ba758310ceb7bba22279
Дмитрий <kriukov@pos-credit.ru>
2024-05-07 14:55:05 +0300

    completed second function

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,13 @@
-from typing import List
-
-def function_of_filtering_words_by_key(word_list: list,
-                                       state='EXECUTED') -> list:
+def filtering_words_by_key(word_list: list, state: str = "EXECUTED") -> list:
     """Функция фильтрует список словарей по ключу state"""
     new_list = []
     for word in word_list:
-        if word.get('state') == state:
+        if word.get("state") == state:
             new_list.append(word)
     return new_list
 
 
-
-
-
-
+def sort_descending(word_list: list, direction: bool = True) -> list:
+    """Функция сортирует словари по дате"""
+    sorted_list = sorted(word_list, key=lambda x: x["date"], reverse=direction)
+    return sorted_list
