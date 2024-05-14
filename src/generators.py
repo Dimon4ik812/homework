c1d2f1d6a808f850e8ffaba5a2f6afc442ddf232
Дмитрий <kriukov@pos-credit.ru>
2024-05-14 20:43:57 +0300

    final version

--- src/generators.py
+++ src/generators.py
@@ -1,105 +1,22 @@
-from typing import List, Dict, Generator
+from typing import List, Generator
 
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
 
-def filter_by_currency(transactions: List[dict], currency: str)-> str:
+def filter_by_currency(transactions: List[dict], currency: str) -> Generator:
     """Функция принимает список словарей и фильтрует его по валюте"""
     for transaction in transactions:
-        if transaction['operationAmount']['currency']['code'] == currency:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
 
 
-
-def transaction_descriptions(transactions: List[dict]) -> str:
+def transaction_descriptions(transactions: List[dict]) -> Generator:
     """Функция возвращает описание каждой операции по очереди"""
     for transaction in transactions:
-        yield transaction['description']
+        yield transaction["description"]
 
 
-
-def card_number_generator(start: int, end: int ) -> Generator:
+def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
     """Функция генерирует номер банковской карты"""
-    for num in range(start, end+1):
-        card_number = '{:016d}'.format(num)
-        formatted_card_number = ' '.join([card_number[i:i+4] for i in range(0, len(card_number), 4)])
+    for num in range(start, end + 1):
+        card_number = "{:016d}".format(num)
+        formatted_card_number = " ".join([card_number[i: i + 4] for i in range(0, len(card_number), 4)])
         yield formatted_card_number
-
-
--- src/widget.py
+++ src/widget.py
@@ -3,12 +3,12 @@ from src.masks import account_disguise, masking_card_number
 
 def data_masking_function(user_data: str) -> str:
     """функция принимает тип карты/счета и номер/карты счета и маскирует их"""
-    if 'Счет'.lower() in user_data.lower():
+    if "Счет".lower() in user_data.lower():
         result = account_disguise(user_data)
         return result
     else:
-        card_type = ''
-        card_number = ''
+        card_type = ""
+        card_number = ""
         for i in user_data:
             if i.isdigit():
                 card_number += i
@@ -19,9 +19,7 @@ def data_masking_function(user_data: str) -> str:
         return card_type + filter_card_number
 
 
-
 def date_decoding(encrypted_date: str) -> str:
     """функция принимает дату и переворачивает ее"""
-    data_parts = encrypted_date.split('T')[0].split('-')
-    return '.'.join(data_parts[::-1])
-
+    data_parts = encrypted_date.split("T")[0].split("-")
+    return ".".join(data_parts[::-1])
